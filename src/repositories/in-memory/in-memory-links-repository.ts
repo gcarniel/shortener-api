956aa1f1b0755b7d082d954cf1c7415949c10a80
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-22 22:08:03 -0300

    fix: default link to create in memory repository

--- src/repositories/in-memory/in-memory-links-repository.ts
+++ src/repositories/in-memory/in-memory-links-repository.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto'
 import { ILink } from '../../interfaces/links/create-link-interface'
 import { LinksRepositoryInterface } from '../interfaces/links-repository-interface'
 
@@ -5,8 +6,20 @@ export class InMemoryLinksRepository implements LinksRepositoryInterface {
   items: ILink[] = []
 
   async create(data: ILink) {
-    this.items.push(data)
+    const newLink: ILink = {
+      id: randomUUID(),
+      code: data.code,
+      url: data.url,
+      shortUrl: data.shortUrl,
+      userId: data.userId || null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deletedAt: null,
+      visits: 0,
+    }
 
-    return data
+    this.items.push(newLink)
+
+    return newLink
   }
 }
