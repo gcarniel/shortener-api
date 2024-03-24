d8cb6b2aaab3415cf217002c2a3dc722a5f36f8e
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-24 15:29:07 -0300

    chore: try-catch list links controller

--- src/http/controllers/links/list-links.ts
+++ src/http/controllers/links/list-links.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 import { makeListLinks } from '../../../use-cases/links/factories/make-list-links'
+import { UserNotFoundError } from '../../../use-cases/users/errors/user-not-found-error'
 
 export async function listLinks(req: FastifyRequest, rep: FastifyReply) {
   const querySchema = z.object({
@@ -18,12 +19,19 @@ export async function listLinks(req: FastifyRequest, rep: FastifyReply) {
     pageSize,
   }
 
-  const resp = await listLinks.execute({ userId, params })
+  try {
+    const resp = await listLinks.execute({ userId, params })
 
-  return rep.status(200).send({
-    page: resp.page,
-    pages: resp.pages,
-    count: resp.count,
-    data: resp.data,
-  })
+    return rep.status(200).send({
+      page: resp.page,
+      pages: resp.pages,
+      count: resp.count,
+      data: resp.data,
+    })
+  } catch (error) {
+    if (error instanceof UserNotFoundError) {
+      return rep.status(404).send({ message: 'User not found' })
+    }
+    throw error
+  }
 }
