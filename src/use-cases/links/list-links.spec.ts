692ff87ed3728192d1b2314fdf4f9754056c63d1
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-24 12:06:32 -0300

    test: add more test in list link

--- src/use-cases/links/list-links.spec.ts
+++ src/use-cases/links/list-links.spec.ts
@@ -2,6 +2,7 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryLinksRepository } from '../../repositories/in-memory/in-memory-links-repository'
 import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository'
 import { ListLinksUseCase } from './list-links'
+import { UserNotFoundError } from '../users/errors/user-not-found-error'
 
 describe('List Links Use Case', () => {
   let linkRepository: InMemoryLinksRepository
@@ -70,5 +71,18 @@ describe('List Links Use Case', () => {
     expect(result.data).toHaveLength(2)
     expect(result.count).toEqual(12)
     expect(result.pages).toEqual(2)
+  })
+
+  it('should not be able to list links that does not belong to the user', async () => {
+    expect(
+      async () =>
+        await sut.execute({
+          userId: 'user-2',
+          params: {
+            page: 1,
+            pageSize: 10,
+          },
+        }),
+    ).rejects.toBeInstanceOf(UserNotFoundError)
   })
 })
