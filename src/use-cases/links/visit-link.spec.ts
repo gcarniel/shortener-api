be3d3a8c8488ce5ed7be0f635a0d4b7f8e85d959
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-24 15:24:21 -0300

    test: removed test that validates user in a link

--- src/use-cases/links/visit-link.spec.ts
+++ src/use-cases/links/visit-link.spec.ts
@@ -2,16 +2,13 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryLinksRepository } from '../../repositories/in-memory/in-memory-links-repository'
 import { VisitLinkUseCase } from './visit-link'
 import { LinkNotFoundError } from './errors/link-not-found-error'
-import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository'
 
 describe('Visit Link Use Case', () => {
   let linkRepository: InMemoryLinksRepository
-  let usersRepository: InMemoryUsersRepository
   let sut: VisitLinkUseCase
 
   beforeEach(() => {
     linkRepository = new InMemoryLinksRepository()
-    usersRepository = new InMemoryUsersRepository()
     sut = new VisitLinkUseCase(linkRepository)
   })
 
@@ -53,49 +50,5 @@ describe('Visit Link Use Case', () => {
     }
 
     expect(amount).toEqual(10)
-  })
-
-  it('should be possible to only visit links created by the user', async () => {
-    const userOne = await usersRepository.create({
-      name: 'User-1',
-      email: 'user1@a.com',
-      password: '123456',
-    })
-
-    const userTwo = await usersRepository.create({
-      name: 'User-2',
-      email: 'user2@a.com',
-      password: '123456',
-    })
-
-    const linkOne = await linkRepository.create({
-      code: 'code12',
-      url: 'https://www.google.com',
-      shortUrl: 'short-url',
-      userId: userOne.id,
-    })
-
-    const linkTwo = await linkRepository.create({
-      code: 'code34',
-      url: 'https://www.google.com',
-      shortUrl: 'short-url',
-      userId: userTwo.id,
-    })
-
-    expect(
-      async () =>
-        await sut.execute({
-          code: linkOne.code,
-          userId: userTwo.id,
-        }),
-    ).rejects.toBeInstanceOf(LinkNotFoundError)
-
-    expect(
-      async () =>
-        await sut.execute({
-          code: linkTwo.code,
-          userId: userOne.id,
-        }),
-    ).rejects.toBeInstanceOf(LinkNotFoundError)
   })
 })
