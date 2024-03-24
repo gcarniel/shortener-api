50f9c83daddcb061ce574cdaa6ea390877648336
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-24 12:06:04 -0300

    test: add more tests in delete link

--- src/use-cases/links/delete-link.spec.ts
+++ src/use-cases/links/delete-link.spec.ts
@@ -2,6 +2,8 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryLinksRepository } from '../../repositories/in-memory/in-memory-links-repository'
 import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository'
 import { DeleteLinkUseCase } from './delete-link'
+import { UserNotFoundError } from '../users/errors/user-not-found-error'
+import { LinkNotFoundError } from './errors/link-not-found-error'
 
 describe('Delete Link Use Case', () => {
   let linkRepository: InMemoryLinksRepository
@@ -34,5 +36,32 @@ describe('Delete Link Use Case', () => {
     })
 
     expect(deletedLink?.deletedAt).not.toBeNull()
+  })
+
+  it('should not be able to delete a link that does not exist', async () => {
+    await usersRepository.create({
+      id: 'user-1',
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+    })
+
+    expect(
+      async () =>
+        await sut.execute({
+          userId: 'user-1',
+          id: 'link-1',
+        }),
+    ).rejects.toBeInstanceOf(LinkNotFoundError)
+  })
+
+  it('should not be able to delete a link that does not belong to the user', async () => {
+    expect(
+      async () =>
+        await sut.execute({
+          userId: 'user-2',
+          id: 'link-1',
+        }),
+    ).rejects.toBeInstanceOf(UserNotFoundError)
   })
 })
