a548379e8eedda3f24db58916b181e683ed5e80c
Gabriel Carniel <gpcarniel@outlook.com>
2024-03-24 12:04:49 -0300

    fix: try catch in create controller

--- src/http/controllers/links/create-link.ts
+++ src/http/controllers/links/create-link.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 import { makeCreateLink } from '../../../use-cases/links/factories/make-create-link'
+import { InvalidUrlError } from '../../../use-cases/links/errors/invalid-url-error'
 
 export async function createLink(req: FastifyRequest, rep: FastifyReply) {
   const bodySchema = z.object({
@@ -12,7 +13,15 @@ export async function createLink(req: FastifyRequest, rep: FastifyReply) {
   const createLink = makeCreateLink()
 
   const userId = req.user?.sub
-  const { link } = await createLink.execute({ url, userId })
+  try {
+    const { link } = await createLink.execute({ url, userId })
 
-  return rep.status(201).send({ link })
+    return rep.status(201).send({ link })
+  } catch (error) {
+    if (error instanceof InvalidUrlError) {
+      return rep.status(400).send({ message: 'Invalid url' })
+    }
+
+    throw error
+  }
 }
